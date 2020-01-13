d9a86f135cc0700e9bbaffd07bfce8068ddb1302
Aleksander Lavrov <lavrov@selectel.ru>
2020-01-14 02:00:40 +0300

    change domain model

--- adonai/domain/crud.py
+++ adonai/domain/crud.py
@@ -1,6 +1,25 @@
 from ..app.crud import CRUDBase
 from .models import Domain
 
+import typing
+from sqlalchemy.orm.session import Session
+
 
 class DomainCRUD(CRUDBase):
     model = Domain
+
+    @classmethod
+    def delete(
+        cls, session: Session, id: int, commit: bool = True, autoflush: bool = False
+    ) -> typing.Optional[bool]:
+        obj = cls.get(session, id)
+
+        if obj:
+            if autoflush:
+                session.flush()
+
+            if commit:
+                obj.is_active = False
+                session.commit()
+
+            return True
--- adonai/domain/models.py
+++ adonai/domain/models.py
@@ -9,3 +9,5 @@ class Domain(db.Model):
     uuid = db.Column(UUID(as_uuid=True), nullable=False, unique=True, default=uuid4)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.Text)
+
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
